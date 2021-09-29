f70a8cd51245e88d53e768d203dabea33ab491f9
Anuvrat <anuvrats06@gmail.com>
2021-09-29 13:58:43 +0530

    managing structs with factories

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,17 @@
-use rand::prelude::*;
-use std::env;
-///This function generates a float number using a number generator passed intp the function.
-///
-/// # Arguments
-/// * generator (&mut ThreadRng): the random number
-/// generator to generate the random number
-///
-/// # Returns
-/// (f64): random number between 0 -> 10
-fn generate_float(generator: &mut ThreadRng) -> f64 {
-    let placeholder: f64 = generator.gen();
-    placeholder * 10.0
-}
+mod to_do;
 
-/// This tarit defines the struct to be a user.
-trait IsUser {
-    /// This function proclaims that the struct is a user.
-    ///
-    /// # Arguments
-    /// None
-    ///
-    /// # Returns
-    /// (bool) true if user, false if not
-    fn is_user() -> bool {
-        true
-    }
-}
-
-/// This struct deines a user
-///
-/// # Attributes
-/// * name (String): the name of the user
-/// * age (i8): the age of the user
-struct User {
-    name: String,
-    age: i8,
-}
+use to_do::{to_do_factory, ItemTypes};
 
 fn main() {
-    let mut rng: ThreadRng = rand::thread_rng();
-    let random_number = generate_float(&mut rng);
-
-    let args: Vec<String> = env::args().collect();
-    let path: &str = &args[0];
-
-    if path.contains("/debug/") {
-        println!("The development server is running.");
-    } else if path.contains("/release/") {
-        println!("The production server is running.")
-    } else {
-        panic!("the seeting is neither debug or release");
+    let to_do_item: Result<ItemTypes, &'static str> = to_do_factory("pending", "make");
+    match to_do_item.unwrap() {
+        ItemTypes::Pending(item) => println!(
+            "It's a pending item with the title: {}",
+            item.super_struct.title
+        ),
+        ItemTypes::Done(item) => println!(
+            "It's a done item with the title: {}",
+            item.super_struct.title
+        ),
     }
-
-    println!("Random number is: {}", random_number);
 }
