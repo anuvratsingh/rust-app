f8044e54275e430e52541bc6aee80bb1307f537c
Anuvrat <anuvrats06@gmail.com>
2021-09-29 15:07:55 +0530

    Interacting with env *confused*

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
-mod to_do;
+mod state;
 
-use to_do::{structs::traits::create::Create, to_do_factory, ItemTypes};
+use std::env;
+use state::{write_to_file, read_file};
+use serde_json::{Map, json, value::{Value}};
+// use text_colorizer::*;
 
 fn main() {
-    let to_do_item: Result<ItemTypes, &'static str> = to_do_factory("pending", "washing");
-    match to_do_item.unwrap() {
-        ItemTypes::Pending(item) => item.create(&item.super_struct.title),
-        ItemTypes::Done(item) => println!(
-            "It's a done item with the title: {}",
-            item.super_struct.title
-        ),
-    }
+    let args: Vec<String> = env::args().collect();
+    let status: &String = &args[1];
+    let title: &String = &args[2];
+
+    let mut state: Map<String, Value> = read_file(&String::from("./state.json"));
+    println!("{:?}", state);
+
+    state.insert(title.to_string(), json!(status));
+    write_to_file("./state.json", &mut state);
+
 }
