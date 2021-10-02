0726562c47b3f7c5b390b6aacea221b7b156828b
Anuvrat <anuvrats06@gmail.com>
2021-10-02 14:53:35 +0530

    join! vs await

--- src/main.rs
+++ src/main.rs
@@ -19,30 +19,39 @@
 //     .await
 // }
 
-use std::thread::JoinHandle;
+use futures::executor::block_on;
+use futures::join;
 use std::{thread, time};
 
-fn do_something(num: i8) -> i8 {
+async fn do_something(num: i8) -> i8 {
     println!("number {} is running", num);
-
     let two_seconds = time::Duration::new(2, 0);
     thread::sleep(two_seconds);
     2
 }
 
 fn main() {
-    let now = time::Instant::now();
-    let thread_one: JoinHandle<i8> = thread::spawn(|| do_something(1));
-    let thread_two: JoinHandle<i8> = thread::spawn(|| do_something(2));
-    let thread_three: JoinHandle<i8> = thread::spawn(|| do_something(3));
+    // let future_one = do_something(1);
+    // let outcome = block_on(future_one);
+    // A bit verbose but gives extra functionality
+    // let future_one = async { do_something(2).await };
+    // let future_one = block_on(future_one);
+    // println!("Here is the outcome for 2 {:?},", future_one);
+    // Two futures in an async block
+    let future_two = async {
+        join!(do_something(3),do_something(4))
+    };
+    let now_two = time::Instant::now();
+    let future_two = block_on(future_two);
+    println!("Here is the outcome for three {:?}", future_two);
+    println!("Time elapsed {:?}", now_two.elapsed());
 
-    let result_one = thread_one.join();
-    let result_two = thread_two.join();
-    let result_three = thread_three.join();
+    let future_three = async {
+        [do_something(5).await, do_something(6).await]
+    };
+    let now_three = time::Instant::now();
+    let future_three = block_on(future_three);
+    println!("Here is the outcome for three {:?}", future_three);
+    println!("Time elapsed {:?}", now_three.elapsed());
 
-    println!("time elapsed {:?}", now.elapsed());
-    println!(
-        "result {}",
-        result_one.unwrap() + result_two.unwrap() + result_three.unwrap()
-    );
 }
